bcb4f9bbcd26e234f6e092ef36b7c5aacda46c27
yangfeng <feng.yang@motphys.com>
2023-09-06 19:42:18 +0800

    Initial Commit

---
+++ Arche-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Arche-cpp
+//
+//  Created by 杨丰 on 2023/9/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
